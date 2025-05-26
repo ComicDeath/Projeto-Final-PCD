2765580f7224c0123febabcb160864f998f47728
Comic <jhlgzelda@gmail.com>
2025-05-26 16:08:11 -0300

    ERRO MEU PERDAO

--- gui.py
+++ gui.py
@@ -5,7 +5,7 @@ janela.title("Plasmid Computer Decoder")
 janela.geometry("500x350")
 
 frameCnt = 9 
-frames = [tk.PhotoImage(file="C:/Users/joao25017/OneDrive - ILUM ESCOLA DE CIÊNCIA/Área de Trabalho/pcd_projeto/Projeto-Final-PCD/assets/12.gif", format=f"gif -index {i}") for i in range(frameCnt)]
+frames = [tk.PhotoImage(file="Projeto-Final-PCD/assets/12.gif", format=f"gif -index {i}") for i in range(frameCnt)]
 
 def gif(label, ind=0):
     frame = frames[ind]
