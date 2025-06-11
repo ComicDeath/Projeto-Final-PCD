f594a6380c68dcd33ebc750706f94111f9b208b8
Comic <jhlgzelda@gmail.com>
2025-06-11 09:35:41 -0300

    Mais bugfixing

--- main.py
+++ main.py
@@ -19,14 +19,14 @@ def capturaDados(entrada, resultado):
     sequencia_crua = entrada.get().strip().replace("\n","").replace(" ","").replace(",","").replace(".","").replace(";","").replace("?","").upper()
     valido = True
     bases_esperadas = ["A","T","C","G"]
-    for letra in sequencia:
+    for letra in sequencia_crua:
         if letra not in bases_esperadas:
             valido = False
             break
     if valido is True:
         with open("assets/sequencia.txt", "w") as f:
-            f.write(sequencia)
-            saida = f"Sequência armazenada: {sequencia}"
+            f.write(sequencia_crua)
+            saida = f"Sequência armazenada: {sequencia_crua}"
     else:
         saida = "Base inesperada identificada. Revise a sua sequência."
     resultado.config(text=saida)
@@ -88,3 +88,13 @@ def enzimas_de_restricao(sequencia):
         enzimas_restricao_tabela += f"\n{enzima}\t{frequencia}"
     return enzimas_restricao_tabela
 
+def grafico_cg_at(sequencia): 
+    plt.close()
+    #rotulos
+    percentuais = [calcular_gc(sequencia), calcular_at(sequencia)]
+    pares = ["GC", "AT"]
+
+    #código do gráfico
+    plt.pie(percentuais, labels = pares, autopct = '%1.1f%%')
+    plt.title("Conteúdo GC vs AT")
+    plt.show()
