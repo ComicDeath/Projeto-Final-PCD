a03367a5e9b942520707658ed66430bbc72115ec
Sarah Silva <sarahsantossilva0501@gmail.com>
2025-06-11 11:16:17 -0300

    atualiza bibliotecas e main

--- main.py
+++ main.py
@@ -157,3 +157,38 @@ def reconhece_proteinas(sequencia):
         proteinas[posicao] = prot
 
     return proteinas
+
+from bibliotecas import antibioticos, genes_resistencia
+
+#Função para identificar os genes de resistencia e sua posição no genoma
+def identifica_genes_resistencia(sequencia_de_bases):
+
+    #O replace é para garantir que não terão espaos atrapalhando
+    sequencia_de_bases = sequencia_de_bases.replace('\n', '')
+    resultados = []
+
+    #O for navega pelos itens do gene_resistencia da biblioteca
+    for nome_gene, sequencia_gene in genes_resistencia.items():
+        posicao = sequencia_de_bases.find(sequencia_gene)
+
+        #O While para ele continuar identificando gene possíveis
+        while posicao != -1:
+            tamanho = len(sequencia_gene)
+            antibiotico = antibioticos[nome_gene]
+            resultados.append({
+                'gene': nome_gene,
+                'posicao': posicao,
+                'tamanho': tamanho,
+                'antibiotico': antibiotico
+            })
+
+            #Posição atualiza a variável para buscar o próximo gene de resistencia
+            posicao = sequencia_de_bases.find(sequencia_gene, posicao + 1)
+
+    # Testa se a lista resultados não está vazia indicando que um gene foi encontrado.
+    if resultados:
+        return resultados
+    
+    #Caso nenhum gene seja encontrado retorna a frase abaixo.
+    else:
+        return "No seu plasmídio não há genes de resistência"
