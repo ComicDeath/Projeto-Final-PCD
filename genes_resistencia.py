7e384cf5a6e36c5363ae1f4056ccc06183789f94
mncunha <mncunha2506@gmail.com>
2025-06-18 08:27:48 -0300

    mudei a mensagem dos genes de resistência

--- genes_resistencia.py
+++ genes_resistencia.py
@@ -34,7 +34,7 @@ def identifica_genes_resistencia(sequencia_de_bases):
     
     #Caso nenhum gene seja encontrado retorna a frase abaixo.
     else:
-        return "No seu plasmídio não há genes de resistência"
+        return "Não há genes de resistência no seu plasmídeo"
 
 # Testando
 resultados = identifica_genes_resistencia(sequencia_de_bases)
