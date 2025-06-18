fab364153bf85329cb8c36b4998c4193ec31cd96
Brenda Abrunhosa <brendabrunhosa@gmail.com>
2025-06-18 08:51:52 -0300

    passei o gráfico de barras para o main

--- main.py
+++ main.py
@@ -198,4 +198,19 @@ def identifica_genes_resistencia(sequencia_de_bases):
     #Caso nenhum gene seja encontrado retorna a frase abaixo.
     else:
         resultado.config(text="No seu plasmídio não há genes de resistência")
-    
+
+def graficobarras(entrada, resultado):
+    nome_das_bases = ["Adenina", "Guanina", "Timina", "Citosina"]
+    lista_de_quantidades = [conta_A, conta_G, conta_T, conta_C]
+    if capturaDados(entrada, resultado) != "Erro":
+        plt.close()
+
+        plt.figure(dpi=240)
+
+        plt.bar(nome_das_bases, lista_de_quantidades, width=0.8)
+
+        plt.title('Quantidade de cada base')
+        plt.xlabel('Bases nitrogenadas')
+        plt.ylabel('Número de bases')
+
+        plt.show()
